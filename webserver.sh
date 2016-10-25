d298af8dc8f9c93dc8b1800ab63c24ab921c0c15
nobletrout <nobletrout@freakazoid.lemure.net>
2016-10-25 09:41:32 -0400

    make this into a real thing

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 # replace site1 and site2 with your website.
 # use gzip compression and embedded images ftw
+site1="example.com"
+site2="example.net"
+rm -f phil
+
 mkfifo phil
-while (`true`); do sudo nc -w 7 -l -p 80 < phil | grep --line-buffered 'Host: .*' | awk '/site1/ {system("cat site1.bytes"); exit 0}; /site2/ {system("cat site2.bytes"); exit 0} {print "NO THANK YOU"; exit 0; } ' > phil; done
+while (`true`)
+  do
+  sudo nc -w 7 -l -p 80 < phil | grep --line-buffered 'Host: .*' | 
+  awk -v site1="$site1" -v site2="$site2" '{ 
+                         if (match($0,site1)) 
+                            {system("cat site1.bytes"); exit 0} 
+                         else if(match($0, site2)) 
+                            {system("cat site2.bytes"); exit 0} 
+                         else 
+                            {print "NO THANK YOU"; exit 0; } 
+                         }' > phil
+  done
